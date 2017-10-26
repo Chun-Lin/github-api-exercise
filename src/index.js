9e23c5b5edbf03d1aa69e3f457c1869ad3cd30f6
Chun-Lin <wulin40063@gmail.com>
2017-10-26 18:04:26 +0800

    refactor($index): convert index.js code from generator-version to async-await-version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import co from 'co';
 import { getRepos } from './api';
 import { getOwnersAndFollowers } from './handlers';
 import { renderOwnerAndFollowers } from './render';
 
-function* searchProjectHandler(keyword) {
-    const repos = yield getRepos(keyword);
-    const ownersWithFollowers = yield Promise.all(getOwnersAndFollowers(repos));
+async function searchProjectHandler(keyword) {
+    const repos = await getRepos(keyword);
+    const ownersWithFollowers = await Promise.all(getOwnersAndFollowers(repos));
     ownersWithFollowers.map(ownerAndFollowers => renderOwnerAndFollowers(ownerAndFollowers));
 }
 
-co(searchProjectHandler('test')).catch(err => console.error('error', err));
+searchProjectHandler('test').catch(err => console.error('error', err));
